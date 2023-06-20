76e21d5f68b0a75e3472bb664b60641f3dd32955
BARINOV228 <136715066+BARINOV228@users.noreply.github.com>
2023-06-20 13:12:30 +0500

    Update Program.cs
    Adde generic type

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
-﻿using shakillar;
+using shakillar;
 
-Square mySquare = new Square(5);
+Square<double> mySquare = new Square<double>(5);
 Console.WriteLine("Площадь квадрата: " + mySquare.GetArea());
 
-Rectangle myRectangle = new Rectangle(6, 3);
+Rectangle<double> myRectangle = new Rectangle<double>(6, 3);
 Console.WriteLine("Площадь прямоугольника: " + myRectangle.GetArea());
 
-Rhombus myRhombus = new Rhombus(8, 6);
+Rhombus<double> myRhombus = new Rhombus<double>(8, 6);
 Console.WriteLine("Площадь ромба: " + myRhombus.GetArea());
 
-Circle myCircle = new Circle(4);
+Circle<double> myCircle = new Circle<double>(4);
 Console.WriteLine("Площадь круга: " + myCircle.GetArea());
+
+Console.WriteLine("Works");
 
 Console.ReadKey();
